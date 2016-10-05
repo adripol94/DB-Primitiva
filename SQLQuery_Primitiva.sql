261b6e58d37d1443a617a3ec58c0b03fa1460e43
Adrian Pol <adripol1994@gmail.com>
2016-10-05 13:46:16 +0200

    Base de datos inicial
    05/10/2016 13:46
    Cambios de la base de datos inicial

--- SQLQuery_Primitiva.sql
+++ SQLQuery_Primitiva.sql
@@ -10,33 +10,32 @@ S_Complementario tinyint NULL
 )
 GO
 CREATE TABLE Boleto(
-B_Id smallint IDENTITY PRIMARY KEY,  -- se calcula comprobando el ultimo id de boleto en sorteo.
+B_Id smallint IDENTITY,
 B_IdSorteo smallint CONSTRAINT FK_BoletoSorteo FOREIGN KEY (B_IdSorteo) REFERENCES Sorteo(S_Id),
 B_Reintegro tinyint NOT NULL,
-B_T_Apuesta varchar(10) not null
+B_TipoApuesta varchar(10) not null,
+CONSTRAINT PK_Boleto  PRIMARY KEY (B_Id, B_IdSorteo) --hay que reiniciar el identity del idBoleto cuando añadamos un nuevo sorteo (Con DBCC CHECKIDENT)
 )
 GO
-
+/*
 CREATE TABLE Numero(
 N_Numero tinyint PRIMARY KEY
-)
+)*/
 
 GO
 CREATE TABLE Boleto_Numero(
-CONSTRAINT PK_Boleto_Numero PRIMARY KEY (BN_IdBoleto, BN_Numero_Bol, BN_IdSorteo),
 BN_IdSorteo smallint not null,
 BN_IdBoleto smallint not null,
 BN_Numero_Bol tinyint not null,
-CONSTRAINT FK_Boleto FOREIGN KEY (BN_IdBoleto) REFERENCES Boleto (B_Id),
-CONSTRAINT FK_Numero_Bol FOREIGN KEY (BN_Numero_Bol) REFERENCES Numero(N_Numero),
-CONSTRAINT FK_IdSorteo FOREIGN KEY (BN_IdSorteo) REFERENCES Boleto(B_IdSorteo)
+CONSTRAINT PK_Boleto_Numero PRIMARY KEY (BN_IdBoleto, BN_Numero_Bol, BN_IdSorteo),
+CONSTRAINT FK_Boleto FOREIGN KEY (BN_IdBoleto, BN_IdSorteo) REFERENCES Boleto (B_Id, B_IdSorteo)
+
 )
 
 GO
 CREATE TABLE Numero_Sorteo(
-CONSTRAINT PK_Numero_Sorteo PRIMARY KEY (NS_IdSorteo, NS_NumeroSorteo),
 NS_IdSorteo smallint not null,
 NS_NumeroSorteo tinyint not null,
-CONSTRAINT FK_Sorteo FOREIGN KEY (NS_IdSorteo) REFERENCES Sorteo (S_Id),
-CONSTRAINT FK_Numero_Sor FOREIGN KEY (NS_NumeroSorteo) REFERENCES Numero (N_Numero)
+CONSTRAINT PK_Numero_Sorteo PRIMARY KEY (NS_IdSorteo, NS_NumeroSorteo),
+CONSTRAINT FK_Sorteo FOREIGN KEY (NS_IdSorteo) REFERENCES Sorteo (S_Id)
 )
